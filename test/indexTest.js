0063ae0c3ca4620519716fab888b9ef9f95dc832
treneese <treneesejohnson9012@gmail.com>
2024-03-20 18:15:35 -0400

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,7 @@
 require ( './helpers.js' );
 
-describe('index.js', function () {
-  describe('cats', function () {
+describe('Array functions', function () {
+  beforeEach(function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
       expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
     });
